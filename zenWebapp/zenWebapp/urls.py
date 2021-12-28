69df839a17e0091d5da13dcf0448ea77115adbd8
Tyler Dickens <tyler.william.dickens@gmail.com>
2021-12-28 08:20:53 -0800

    refactored for static files and templates

--- zenWebapp/zenWebapp/urls.py
+++ zenWebapp/zenWebapp/urls.py
@@ -20,6 +20,6 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard/', include('dashboard.urls')),
+    path('', include('dashboard.urls')),
     path('login/', include('login.urls')),
 ]
