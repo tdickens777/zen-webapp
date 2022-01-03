12465f45a1dfc312304f938f4c583b59cbd1bfe5
Tyler Dickens <tyler.william.dickens@gmail.com>
2022-01-02 17:28:36 -0800

    tweaks

--- zenWebapp/login/urls.py
+++ zenWebapp/login/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='login-page'),
+    path('register/', views.register, name="registration-page")
 ]
--- zenWebapp/login/views.py
+++ zenWebapp/login/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'login/index.html')
+
+def register(request):
+    return render(request, 'login/register.html')
